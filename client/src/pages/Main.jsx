c7e70ce827fb0ec329430ba8e14621ab577a3725
Burak849 <burakkurtulus018@gmail.com>
2023-06-21 02:05:15 +0300

    update

---
+++ client/src/pages/Main.jsx
@@ -0,0 +1,299 @@
+import React from 'react'
+import Navbar from '../components/Navbar' 
+import Hero  from '../components/Hero' 
+import Logo from './uber.png'
+import { useEffect, useState } from 'react'
+
+
+export default () => {
+  const features = [
+    {
+        icon:
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+            </svg>,
+        title: "Fast Refresh",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+    },
+    {
+        icon:
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+            </svg>,
+        title: "Analytics",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+    },
+    {
+        icon:
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+            </svg>,
+        title: "Datacenter security",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+    },
+    {
+        icon:
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
+            </svg>,
+        title: "Build on your terms",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+    },
+    {
+        icon:
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
+            </svg>,
+        title: "Safe to use",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+    },
+    {
+        icon:
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+            </svg>,
+        title: "Flexible",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+    },
+]
+
+    const [state, setState] = useState(false)
+
+    // Replace javascript:void(0) paths with your paths
+    const navigation = [
+        { title: "Ana Sayfa", path: "/" },
+        { title: "Hakkımda", path: "/about" },
+        { title: "Özelliklerimiz", path: "#stats" },
+        { title: "Yorumlar", path: "#comments" },
+        { title: "Hakkımızda", path: "#about" }
+    ]
+    const testimonials = [
+      {
+          avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
+          name: "Martin escobar",
+          title: "Founder of meta",
+          quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et est hendrerit, porta nunc vitae."
+      },
+      {
+          avatar: "https://randomuser.me/api/portraits/women/79.jpg",
+          name: "Angela stian",
+          title: "Product designer",
+          quote: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."
+      },
+      {
+          avatar: "https://randomuser.me/api/portraits/men/86.jpg",
+          name: "Karim ahmed",
+          title: "DevOp engineer",
+          quote: "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain."
+      },
+  ]
+
+    useEffect(() => {
+        document.onclick = (e) => {
+            const target = e.target;
+            if (!target.closest(".menu-btn")) setState(false);
+        };
+    }, [])
+
+
+    const Brand = () => (
+        <div className="flex items-center justify-between py-5 md:block">
+            <a href="javascript:void(0)">
+                <img
+                    src={Logo}
+                    width={120}
+                    height={50}
+                    alt="logo"
+                />
+            </a>
+            <div className="md:hidden">
+                <button className="menu-btn text-gray-500 hover:text-gray-800"
+                    onClick={() => setState(!state)}
+                >
+                    {
+                        state ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                            </svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+                        )
+                    }
+                </button>
+            </div>
+        </div>
+    )
+    return (
+      <div>
+        <div className='relative'>
+            <div className='absolute inset-0 blur-xl h-[580px]' style={{ background: "linear-gradient(143.6deg, rgba(192, 132, 252, 0) 20.79%, rgba(232, 121, 249, 0.26) 40.92%, rgba(204, 171, 238, 0) 70.35%)" }}></div>
+            <div className='relative'>
+                <header>
+                    <div className={`md:hidden ${state ? "mx-2 pb-5" : "hidden"}`}>
+                        <Brand />
+                    </div>
+                    <nav className={`pb-5 md:text-sm ${state ? "absolute top-0 inset-x-0 bg-white shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-0 md:mt-0 md:relative md:bg-transparent" : ""}`}>
+                        <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
+                            <Brand />
+                            <div className={`flex-1 items-center mt-8 md:mt-0 md:flex ${state ? 'block' : 'hidden'} `}>
+                                <ul className="flex-1 justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+                                    {
+                                        navigation.map((item, idx) => {
+                                            return (
+                                                <li key={idx} className="text-gray-700 hover:text-gray-900">
+                                                    <a href={item.path} className="block">
+                                                        {item.title}
+                                                    </a>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                                <div className="items-center justify-end mt-6 space-y-6 md:flex md:mt-0">
+                                    <a href="/login" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
+                                        Giriş yap
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                                            <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                                        </svg>
+                                    </a>
+                                  
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+                <section>
+                    <div className="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 overflow-hidden md:px-8 md:flex">
+                        <div className='flex-none space-y-5 max-w-xl'>
+                            <a href="javascript:void(0)" className='inline-flex gap-x-6 items-center rounded-full p-1 pr-6 border text-sm font-medium duration-150 hover:bg-white'>
+                                <span className='inline-block rounded-full px-3 py-1 bg-indigo-600 text-white'>
+                                    News
+                                </span>
+                                <p className='flex items-center'>
+                                    Read the launch post from here
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                                        <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                                    </svg>
+                                </p>
+                            </a>
+                            <h1 className="text-4xl text-gray-800 font-extrabold sm:text-5xl">
+                                Suber ile güvenli yolculuk yapın
+                            </h1>
+                            <p>
+                                Sed ut perspiciatis unde omnis iste natus voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
+                            </p>
+                            <div className='flex items-center gap-x-3 sm:text-sm'>
+                                <a href="javascript:void(0)" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
+                                    Basla
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                                        <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                                    </svg>
+                                </a>
+                                <a href="javascript:void(0)" className="flex items-center justify-center gap-x-1 py-2 px-4 text-gray-700 hover:text-gray-900 font-medium duration-150 md:inline-flex">
+                                    İletisim
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                                        <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div className='flex-1 hidden md:block'>
+                            {/* Replace with your image */}
+                            <img src="https://images.pexels.com/photos/3787149/pexels-photo-3787149.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="max-w-xl rounded-md" />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <section id="stats" className="py-14">
+        <div className="max-w-screen-xl mx-auto px-4 text-center text-gray-600 md:px-8">
+            <div className="max-w-2xl mx-auto">
+                <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                    The fastest way to launch a product
+                </h3>
+                <p className="mt-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus.
+                </p>
+            </div>
+            <div className="mt-12">
+                <ul className="grid gap-y-8 gap-x-12 sm:grid-cols-2 lg:grid-cols-3">
+                    {
+                        features.map((item, idx) => (
+                            <li key={idx} className="space-y-3">
+                                <div className="w-12 h-12 mx-auto bg-indigo-50 text-indigo-600 rounded-full flex items-center justify-center">
+                                    {item.icon}
+                                </div>
+                                <h4 className="text-lg text-gray-800 font-semibold">
+                                    {item.title}
+                                </h4>
+                                <p>
+                                    {item.desc}
+                                </p>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section  id="comments" className="py-14 bg-gray-800">
+            <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+                <div className="max-w-xl sm:text-center md:mx-auto">
+                    <h3 className="text-white text-3xl font-semibold sm:text-4xl">
+                        See what others saying about us
+                    </h3>
+                    <p className="mt-3 text-gray-400 ">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et est hendrerit, porta nunc vitae, gravida justo. Nunc fermentum magna lorem, euismod volutpat arcu volutpat et.
+                    </p>
+                </div>
+                <div className="mt-12">
+                    <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                        {
+                            testimonials.map((item, idx) => (
+                                <li key={idx} className="bg-gray-50 p-4 rounded-xl">
+                                    <figure>
+                                        <div className="flex items-center gap-x-4">
+                                            <img src={item.avatar} className="w-16 h-16 rounded-full" />
+                                            <div>
+                                                <span className="block text-gray-800 font-semibold">{item.name}</span>
+                                                <span className="block text-gray-600 text-sm mt-0.5">{item.title}</span>
+                                            </div>
+                                        </div>
+                                        <blockquote>
+                                            <p className="mt-6 text-gray-700">
+                                                {item.quote}
+                                            </p>
+                                        </blockquote>
+                                    </figure>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section id="about" className="py-14">
+            <div className="max-w-screen-xl mx-auto md:px-8">
+                <div className="items-center gap-x-12 sm:px-4 md:px-0 lg:flex">
+                    <div className="flex-1 sm:hidden lg:block">
+                        <img src="https://images.livemint.com/img/2019/10/01/1600x900/336921309_0-8_1556337045643_1569922871237.jpg" className="md:max-w-lg sm:rounded-lg" alt="" />
+                    </div>
+                    <div className="max-w-xl px-4 space-y-3 mt-6 sm:px-0 md:mt-0 lg:max-w-2xl">
+                        <h3 className="text-indigo-600 font-semibold">
+                            Hakkımızda
+                        </h3>
+                        <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                            Build your SaaS solution with help from our experts
+                        </p>
+                        <p className="mt-3 text-gray-600">
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum, sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    )
+}
